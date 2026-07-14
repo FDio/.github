4e4519eb2975f94740bb12474861b92e7500acec
Dave Wallace <dwallacelf@gmail.com>
2026-07-14 00:04:25 -0400

    ci(docker): remove jenkins builder image upload from builder script
    - update REAMDE.md to replace jenkins references with gha refs
    
    Change-Id: I84d10999b3f1b9d2c0f346f1ca4d655340c5602c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/build_executor_docker_image.sh
+++ docker/scripts/build_executor_docker_image.sh
@@ -33,8 +33,6 @@ gha_ci_image=""
 os_names=""
 push_to_docker_hub=""
 dump_dockerfile=""
-gha_container=""
-skip_executor_build=""
 executor_docker_image=""
 gha_docker_image=""
 
@@ -43,11 +41,9 @@ usage() {
     echo
     echo "Usage: $0 [-c <class>] [-p] [-r <role>] -a | <os name> [... <os name>]"
     echo "  -a                Run all OS's supported on class $EXECUTOR_CLASS & arch $OS_ARCH"
-    echo "  -b <builder img>  Bake GitHub Action variant from specified builder class image"
     echo "  -c <class>        Default is '$EXECUTOR_DEFAULT_CLASS'"
     executor_list_classes
     echo "  -d                Generate Dockerfile, dump it to stdout, and exit"
-    echo "  -g                Bake GitHub Action variant of builder class"
     echo "  -p                Push docker images to Docker Hub"
     echo "  -r <role>         Add a role based tag (e.g. sandbox-x86_64):"
     executor_list_roles
@@ -56,17 +52,9 @@ usage() {
 }
 
 must_be_run_as_root_or_docker_group
-while getopts ":ab:c:dghpr:" opt; do
+while getopts ":a:c:dhpr:" opt; do
     case "$opt" in
         a)  all_os_names="1" ;;
-        b) if executor_verify_image "$OPTARG" ; then
-               gha_container="1"
-               skip_executor_build="1"
-               executor_docker_image="$OPTARG"
-           else
-               echo "ERROR: Existing executor image '$OPTARG' not found!"
-               usage
-           fi ;;
         c) if executor_verify_class "$OPTARG" ; then
                EXECUTOR_CLASS="$OPTARG"
                EXECUTOR_CLASS_ARCH="$EXECUTOR_CLASS-$OS_ARCH"
@@ -75,7 +63,6 @@ while getopts ":ab:c:dghpr:" opt; do
                usage
            fi ;;
         d) dump_dockerfile="1"; set +x ;;
-        g) gha_container="1" ;;
         h) usage ;;
         p) push_to_docker_hub="1" ;;
         r) if executor_verify_role "$OPTARG" ; then
@@ -117,7 +104,7 @@ for executor_os_name in $os_names ; do
 done
 
 docker_build_setup_gha() {
-    if [ -n "$gha_container" ] && vpp_supported_executor_class ; then
+    if vpp_supported_executor_class ; then
         rm -rf "$DOCKER_GHA_RUNNER_DIR"
         mkdir -p "$DOCKER_GHA_RUNNER_DIR"
         cp "$DOCKER_DOT_GITHUB_GHA_RUNNER_DIR"/* "$DOCKER_GHA_RUNNER_DIR"
@@ -141,9 +128,8 @@ for executor_os_name in $os_names ; do
     os_name="${executor_os_name//-}"
     os_name="${os_name//.}"
     repository="fdiotools/${EXECUTOR_CLASS}-$os_name"
-    if [ -z "$skip_executor_build" ] ; then
-        executor_docker_image="$repository:$DOCKER_TAG"
-    elif ! grep -q "$os_name" <<< "$executor_docker_image" ; then
+    executor_docker_image="$repository:$DOCKER_TAG"
+    if ! grep -q "$os_name" <<< "$executor_docker_image" ; then
         set_opts="$-"
         set +x # disable trace output
         echo "ERROR: OS name '$os_name' missing from executor image specified in '-b $executor_docker_image'!"
@@ -170,41 +156,26 @@ for executor_os_name in $os_names ; do
         echo -e "$line\n"
     else
         docker login
-        if [ -z "$skip_executor_build" ] ; then
-            docker build -t "$executor_docker_image" "$DOCKER_BUILD_DIR"
-            rm -f "$DOCKERFILE"
-        fi
-        if [ -n "$gha_container" ] ; then
-            gha_docker_image="${executor_docker_image/$EXECUTOR_CLASS/gha}"
-            pushd "$DOCKER_GHA_RUNNER_DIR"
-            docker build \
-              --load \
-              --no-cache \
-              --platform linux/"$DEB_ARCH" \
-              --tag "$gha_docker_image" \
-              --build-arg BASE_IMAGE="$executor_docker_image" \
-              --build-arg DOCKER_GHA_RUNNER_DIR="${DOCKER_GHA_RUNNER_DIR}" \
-              -f "${DOCKER_GHA_RUNNER_DIR}/Dockerfile" \
-              "${DOCKER_GHA_RUNNER_DIR}"
-             popd
-        fi
+        docker build -t "$executor_docker_image" "$DOCKER_BUILD_DIR"
+        rm -f "$DOCKERFILE"
+        gha_docker_image="${executor_docker_image/$EXECUTOR_CLASS/gha}"
+        pushd "$DOCKER_GHA_RUNNER_DIR"
+        docker build \
+          --load \
+          --no-cache \
+          --platform linux/"$DEB_ARCH" \
+          --tag "$gha_docker_image" \
+          --build-arg BASE_IMAGE="$executor_docker_image" \
+          --build-arg DOCKER_GHA_RUNNER_DIR="${DOCKER_GHA_RUNNER_DIR}" \
+          -f "${DOCKER_GHA_RUNNER_DIR}/Dockerfile" \
+          "${DOCKER_GHA_RUNNER_DIR}"
+         popd
         if [ -n "$ci_tag" ] ; then
-            ci_image="$repository:$ci_tag"
-            echo -e "\nAdding docker tag $ci_image to $executor_docker_image"
-            docker tag "$executor_docker_image" "$ci_image"
-            if [ -n "$gha_container" ] ; then
-                gha_ci_image="${ci_image/$EXECUTOR_CLASS/gha}"
-                echo -e "\nAdding docker tag $gha_ci_image to $gha_docker_image"
-                docker tag "$gha_docker_image" "$gha_ci_image"
-            fi
+            gha_ci_image="${ci_image/$EXECUTOR_CLASS/gha}"
+            echo -e "\nAdding docker tag $gha_ci_image to $gha_docker_image"
+            docker tag "$gha_docker_image" "$gha_ci_image"
         fi
         if [ -n "$push_to_docker_hub" ] ; then
-            echo -e "\nPushing jenkins docker image ''$executor_docker_image' to Docker Hub..."
-            docker push "$executor_docker_image"
-            if [ -n "$ci_image" ] ; then
-                echo -e "\nPushing jenkins ci image tag '$ci_image' to Docker Hub..."
-                docker push "$ci_image"
-            fi
             if [ -n "$gha_docker_image" ] ; then
                 echo -e "\nPushing gha docker image '$gha_docker_image' to Docker Hub..."
                 docker push "$gha_docker_image"
