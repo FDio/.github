2248014c1584fb460b5153212e0753f79c07d9e9
Peter Mikus <peter.mikus@icloud.com>
2026-09-02 11:19:47 +0200

    feat: Nomad
    Change-Id: I740f7faebe2da5ad575e16107aec43477c74f5ea
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>

--- terraform/nomad-alertmanager/main.tf
+++ terraform/nomad-alertmanager/main.tf
@@ -2,7 +2,7 @@ resource "nomad_job" "nomad_job" {
   jobspec = file("${path.cwd}/conf/nomad/alertmanager.hcl")
   hcl2 {
     vars = {
-        version = "0.34.0"
+      version = "0.34.0"
     }
   }
   detach = false
